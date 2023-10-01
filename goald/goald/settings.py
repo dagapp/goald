d442a88d420aaa8310395faa7d8c4ca78a553090
Zuxigit <shim.01@mail.ru>
2023-10-01 17:43:31 +0300

    Test run server

--- goald/goald/settings.py
+++ goald/goald/settings.py
@@ -77,6 +77,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'app': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'app_db.sqlite3',
+        'USER': 'root'
     }
 }
 
---
+++ goald/goald_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GoaldAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'goald_app'
