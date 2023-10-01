96353acef55f8aeb12959336980aec35b4d5cd2a
Zuxigit <shim.01@mail.ru>
2023-10-01 17:56:42 +0300

    Some repo reorganizations

--- goald_site/asgi.py
+++ goald_site/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for goald project.
+ASGI config for goald_site project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goald.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goald_site.settings')
 
 application = get_asgi_application()
