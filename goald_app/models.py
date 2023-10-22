b0111cda9cd9e811b8ba01f489ebd38c85e1ed5a
dagapp <dnagapitov@gmail.com>
2023-10-22 22:22:15 +0300

    Added models Added Goal model and added some field to existing models

--- goald_app/models.py
+++ goald_app/models.py
@@ -14,15 +14,32 @@ class User(models.Model):
 class Group(models.Model):
     name = models.CharField(default="", null=False, max_length=20)
     password = models.CharField(blank=True, null=True, max_length=50)
+    is_public = models.BooleanField(null=False, default=True)
 
     leader_id = models.ForeignKey('User', null=False, on_delete=models.CASCADE)
     goal_id = models.ForeignKey('Goal', null=False, on_delete=models.CASCADE)
 
+    supergroup_id = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+
 
 class Goal(models.Model):
     name = models.CharField(default="", null=False, max_length=50)
-    goal = models.IntegerField(default=0)
+    is_active = models.BooleanField(null=False, default=True)
+    final_value = models.IntegerField(default=0)
     current_value = models.IntegerField(default=0)
     deadline = models.DateTimeField()
+    alert_period = models.TimeField(null=True)
 
     group_id = models.ForeignKey('Group', null=False, on_delete=models.CASCADE)
+    #report_id = models.ForeignKey('Report', null=True, on_delete=models.CASCADE)
+
+    supergoal_id = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+
+
+class Duty(models.Model):
+	amount = models.IntegerField(null=False)
+	deadline = models.DateTimeField()
+	alert_period = models.TimeField()
+
+	user_id = models.ForeignKey('User', null=False, on_delete=models.CASCADE)
+	goal_id = models.ForeignKey('Goal', null=False, on_delete=models.CASCADE)
