39d739b416781f474bb7f25c08faf7d5265edfb6
Timur <Davletshin.TI@phystech.edu>
2023-10-23 12:09:12 +0300

    *bugfix in models

--- goald_app/models.py
+++ goald_app/models.py
@@ -3,78 +3,87 @@ from django.db import models
 # Create your models here.
 
 class UserModel(models.Model):
-    login    = models.CharField  (null=False, max_length=50 )
+    login    = models.CharField  (null=False, max_length=50)
     password = models.BinaryField(null=False)
 
     name        = models.CharField(null=True, max_length=50)
     second_name = models.CharField(null=True, max_length=50)
 
-    #group_id = models.ForeignKey('Group', on_delete=models.CASCADE)
 
-'''
+class GroupModel(models.Model):
+    tag       = models.CharField   (null=False)
+    is_public = models.BooleanField(null=False)
 
-class Group(models.Model):
-    name = models.CharField(default="", null=False, max_length=20)
-    password = models.CharField(blank=True, null=True, max_length=50)
-    is_public = models.BooleanField(null=False, default=True)
+    name     = models.CharField  (null=True, max_length=50)
+    password = models.BinaryField(null=True)
+    image    = models.ImageField (null=True)
 
-    leader_id = models.ForeignKey('User', null=False, on_delete=models.CASCADE)
-    goal_id = models.ForeignKey('Goal', null=False, on_delete=models.CASCADE)
-    members = models.ManyToManyField('User', null=False, on_delete=models.CASCADE)
+    members = models.ManyToManyField('UserModel')
+
+    leader_id = models.ForeignKey('UserModel', null=False, on_delete=models.CASCADE)
+    goal_id   = models.ForeignKey('GoalModel', null=False, on_delete=models.CASCADE)
+
     supergroup_id = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
 
 
-class Goal(models.Model):
-    name = models.CharField(default="", null=False, max_length=50)
+class GoalModel(models.Model):
+    name      = models.CharField   (null=False, max_length=50)
     is_active = models.BooleanField(null=False, default=True)
-    final_value = models.IntegerField(default=0)
-    current_value = models.IntegerField(default=0)
-    deadline = models.DateTimeField()
+
+    deadline = models.DateTimeField(null=True)
     alert_period = models.TimeField(null=True)
 
-    group_id = models.ForeignKey('Group', null=False, on_delete=models.CASCADE)
-    #report_id = models.ForeignKey('Report', null=True, on_delete=models.CASCADE)
+    group_id  = models.ForeignKey('GroupModel' , null=False, on_delete=models.CASCADE)
+    report_id = models.ForeignKey('ReportModel', null=True , on_delete=models.CASCADE)
 
     supergoal_id = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
 
 
-class Duty(models.Model):
-	amount = models.IntegerField(null=False)
-	deadline = models.DateTimeField()
-	alert_period = models.TimeField()
+class DutyModel(models.Model):
+    final_value   = models.IntegerField(null=False)
+    current_value = models.IntegerField(null=False)
 
-	user_id = models.ForeignKey('User', null=False, on_delete=models.CASCADE)
-	goal_id = models.ForeignKey('Goal', null=False, on_delete=models.CASCADE)
+    deadline = models.DateTimeField(null=True)
+    alert_period = models.TimeField(null=True)
+
+    user_id = models.ForeignKey('UserModel', null=False, on_delete=models.CASCADE)
+    goal_id = models.ForeignKey('GoalModel', null=False, on_delete=models.CASCADE)
+
+
+class EventModel(models.Model):
+    type      = models.IntegerField (null=False)
+    text      = models.CharField    (null=False, max_length=500)
+    timestamp = models.DateTimeField(null=False)
+
+    group_id = models.ForeignKey('GroupModel', null=False, on_delete=models.CASCADE)
+    goal_id  = models.ForeignKey('GoalModel' , null=False, on_delete=models.CASCADE)
+
+
+class ReportModel(models.Model):
+    proof = models.ImageField(null=False)
+
+    text = models.CharField (null=True, max_length=500)
+
+    goal_id = models.ForeignKey('GoalModel', null=False, on_delete=models.CASCADE)
+
 '''
-     
-class Event(models.Model):
-    type = models.IntegerField(null=False)
-    text = models.CharField(blank=True, null=False, max_length=500)
-    timestamp = models.DateTimeField()
+class MessageModel(models.Model):
+    text      = models.CharField    (null=False, max_length=500)
+    timestamp = models.DateTimeField(null=False) 
 
-    group_id = models.ForeignKey('Group', null=False, on_delete=models.CASCADE)
-    goal_id = models.ForeignKey('Goal', null=False, on_delete=models.CASCADE)
+    sender_id = models.ForeignKey('UserModel', null=False, on_delete=models.CASCADE)
 
-class Report(models.Model):
-    text = models.CharField(blank=False, null=False, max_length=500)
-    proof = models.BinaryField()
 
-    goal_id = models.ForeignKey('Goal', null=False, on_delete=models.CASCADE)
+class PrivateChatModel(models.Model):
+    messages = models.ManyToManyField('MessageModel')
 
-class Message(models.Model):
-    text = models.CharField(blank=True, null=False, max_length=500)
-    timestamp = models.DateTimeField() 
+    user1_id = models.ForeignKey('UserModel', null=False, on_delete=models.CASCADE)
+    user2_id = models.ForeignKey('UserModel', null=False, on_delete=models.CASCADE)
 
-    sender_id = models.ForeignKey('User', null=False, on_delete=models.CASCADE)
 
-class PrivateChat(models.Model):
-    user_id1 = models.ForeignKey('User', null=False, on_delete=models.CASCADE)
-    user_id2 = models.ForeignKey('User', null=False, on_delete=models.CASCADE)
-
-    messages = models.ManyToManyField('Message', null=False, on_delete=models.CASCADE)
-
-class GroupChat(models.Model):
+class GroupChatModel(models.Model):
     name = models.CharField(blank=False, null=False, max_length=50)
 
-    group_id = models.OneToOneField('Group', null=False, on_delete=models.CASCADE)
-    messages = models.ManyToManyField('Message', null=False, on_delete=models.CASCADE)
+    group_id = models.OneToOneField('GroupModel', null=False, on_delete=models.CASCADE)
+    messages = models.ManyToManyField('MessageModel', null=False, on_delete=models.CASCADE)
+'''
