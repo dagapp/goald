87247161747df5066f9aaeb56792c519d97ad677
zuxigit <shim.01@mail.ru>
2023-12-21 02:56:39 +0300

    Removing useless managers. Refactoring
    Removed all useless managers and merged all handlers into one manager.
    Did a little refactoring.

--- goald_app/views/group/group.py
+++ goald_app/views/group/group.py
@@ -1,38 +1,39 @@
 """
 File for defining handlers for group in Django notation
 """
-
 import json
 
 from django.http import JsonResponse
 
-from goald_app.managers.common import AlreadyExists
-from goald_app.managers.group import GroupManager
-from goald_app.managers.user import UserManager, dataclass_encoder
-from goald_app.managers.image import ImageManager
+from goald_app.manager.exceptions import AlreadyExists
+from goald_app.manager.manager import Manager, DataclassEncoder
 
 
 def create(request):
     """
     Handler to create group
     """
-    if not request.POST:
-        return JsonResponse({
-            "result" : "error", 
-            "message": "wrong HTTP method"
-        })
+    if request.method != "POST":
+        return JsonResponse(
+            {
+                "result": "Bad request",
+                "message": "wrong HTTP method, expected POST"
+            })
 
     data = json.loads(request.body)
 
-    if not "name" in data or not "is_private" in data:
-        return JsonResponse({
-            "result" : "error", 
-            "message": "group_name or privacy_mode doesn't exist in request"
-        })
+    if "name" not in data or "is_private" not in data:
+        return JsonResponse(
+            {
+                "result": "Bad",
+                "message": "group_name or privacy_mode parameter doesn't exist in the request"
+            })
 
     image_path = "group/default.jpg"
+
     if data["image"] != "":
-        image_path = ImageManager.store(request.FILES["image"])
+        image_file = request.FILES["image"]
+        image_path = Manager.store_image(image=image_file)
 
     selected_privacy_mode = data["is_private"]
     is_public = False
@@ -40,31 +41,33 @@ def create(request):
         is_public = True
 
     try:
-        GroupManager.create(
+        Manager.create_group(
             name=data["name"],
             image=image_path,
             leader_id=request.session["id"],
             is_public=is_public,
         )
-    except AlreadyExists:
-        return JsonResponse({
-            "result" : "error", 
-            "message": "group already exists"
-        })
+    except AlreadyExists as e:
+        return JsonResponse(
+            {
+                "result": "Bad",
+                "message": f"Failed to create a group: {e}"
+            })
 
-    return JsonResponse({"result" : "ok"})
+    return JsonResponse({"result": "Ok"})
 
 
 def view(request, group_id):
     """
     Handler to serialize group to json
     """
-    group = UserManager.get_group(user_id=request.session['id'], group_id=group_id)
-    return JsonResponse(data=group, safe=False, encoder=dataclass_encoder)
+    group = Manager.get_user_group(user_id=request.session['id'], group_id=group_id)
+    return JsonResponse(data=group, safe=False, encoder=DataclassEncoder)
+
 
 def list(request):
     """
     Handler to serialize groups to json
     """
-    groups = UserManager.get_groups(user_id=request.session['id'])
-    return JsonResponse(data=groups, safe=False, encoder=dataclass_encoder)
+    groups = Manager.get_user_groups(user_id=request.session['id'])
+    return JsonResponse(data=groups, safe=False, encoder=DataclassEncoder)
