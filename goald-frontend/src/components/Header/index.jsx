a6c3d6f8f659ed3d9af16af9cf1f939e9627a211
uf00led <dmitriimail8@gmail.com>
2024-03-25 11:30:35 +0300

    Adding Header React component. Resolving dependences

---
+++ goald-frontend/src/components/Header/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import logoSvg from "../../assets/logo.svg"
+import './header.scss'
+
+export default function Header() {
+  return (
+    <header>
+        <div className="header__container">
+            <a href="" className="header__logo"><img src={logoSvg} alt="GOALD"/></a>
+            <div className="header__navigation">
+                {/* <nav className="menu">
+                    <ul className="menu__body">
+                        <li className="menu__item"><a href="#" className="menu__link">Features</a></li>
+                        <li className="menu__item"><a href="#" className="menu__link">FAQ</a></li>
+                        <li className="menu__item"><a href="#" className="menu__link">About Us</a></li>
+                    </ul>
+                </nav> */}
+                <div className="header__buttons">
+                    <button className="header__button">Sign In</button>
+                    <button className="header__button gradient_button">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </header>
+  )
+}
