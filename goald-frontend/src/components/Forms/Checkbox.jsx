c48a235a8702cefe0be4a0a4d7b88477c2802349
uf00led <dmitriimail8@gmail.com>
2024-03-28 02:22:23 +0300

    Making styles for Checkbox. Preparing animations for placeholders

--- goald-frontend/src/components/Forms/Checkbox.jsx
+++ goald-frontend/src/components/Forms/Checkbox.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function Checkbox({description}) {
     return (
       <div className="checkbox">
-          <input type="checkbox" className='checkbox__button'/>
-          <span className='checkbox__description'>{description}</span>
+          <input type="checkbox" id='checkbox-id' className='checkbox__input'/>
+          <span className='checkbox__button'></span>
+          <label for="checkbox-id" className='checkbox__label'>{description}</label>
       </div>
     )
   }
