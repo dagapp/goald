e65ba5bcd794bf6fc7d8f79547128b9baf48a581
uf00led <dmitriimail8@gmail.com>
2024-03-30 18:52:52 +0300

    Asset Managment. Сomponents are no longer combined with one style file.

--- goald-frontend/src/components/Forms/Placeholder/index.jsx
+++ goald-frontend/src/components/Forms/Placeholder/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import './placeholder.scss'
+
 export default function Placeholder({description, isPassword = false}) {
     return (
-      <div className="placeholder">
+      <div className='placeholder'>
           <span className="placeholder__description">{description}</span>
-          <input type={isPassword ? "password" : "text"} className='placeholder__field' required />
+          <input type={isPassword ? "password" : "text"} className="placeholder__field" required />
       </div>
     )
 }
--- goald-frontend/src/components/Forms/index.jsx
+++
@@ -1,6 +0,0 @@
-import './forms.scss'
-
-import Checkbox    from './Checkbox'
-import Placeholder from './Placeholder'
-
-export {Checkbox, Placeholder} 
