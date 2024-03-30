82466a7002b3ce746f5176719e298ae397cb42c9
uf00led <dmitriimail8@gmail.com>
2024-03-30 18:50:31 +0300

    Restyling Header. Simplify. Make it easier for Users

--- goald-frontend/src/components/Header/index.jsx
+++ goald-frontend/src/components/Header/index.jsx
@@ -1,25 +1,21 @@
 import React from 'react'
 
 import logoSvg from "../../assets/logo.svg"
+import themeSwitchDark from "../../assets/icons/mode/theme-switch-dark.svg"
+import themeSwitchLight from "../../assets/icons/mode/theme-switch-light.svg"
+
 import './header.scss'
 
 export default function Header() {
   return (
     <header>
         <div className="header__container">
-            <a href="" className="header__logo"><img src={logoSvg} alt="GOALD"/></a>
-            <div className="header__navigation">
-                {/* <nav className="menu">
-                    <ul className="menu__body">
-                        <li className="menu__item"><a href="#" className="menu__link">Features</a></li>
-                        <li className="menu__item"><a href="#" className="menu__link">FAQ</a></li>
-                        <li className="menu__item"><a href="#" className="menu__link">About Us</a></li>
-                    </ul>
-                </nav> */}
-                <div className="header__buttons">
-                    <button className="header__button">Войти</button>
-                    <button className="header__button gradient__button round__button">Регистрация</button>
-                </div>
+            <div className="header__logo">
+                <a href="#" className="header__logo"><img src={logoSvg} alt="GOALD"/></a>
+            </div>
+            
+            <div className="header__theme">
+                <img src={themeSwitchDark} alt="Dark"></img>
             </div>
         </div>
     </header>
