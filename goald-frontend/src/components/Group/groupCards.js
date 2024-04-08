c8eb7ccaf247f6cb0b65fcd9642cda9ab2906c78
uf00led <dmitriimail8@gmail.com>
2024-04-08 09:39:16 +0300

    Adding Group component with test JSON. Fixing Group and Header iteraction

---
+++ goald-frontend/src/components/Group/groupCards.js
@@ -0,0 +1,45 @@
+export const template = {
+    id: 1,
+    title: "Template",
+    description: "Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в болеенедавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.",
+    tags: [
+        {   
+            id: 10,
+            value: "Lorem"
+        },
+        {   
+            id: 20,
+            value: "Ipsum"
+        },
+        {   
+            id: 30,
+            value: "Letraset"
+        }
+    ],
+    goals: [
+        {
+            id: 10,
+            title: "First",
+            daysLeft: 40, 
+            amount: 1000,
+            goal: 2000,
+            percentage: 50,
+        },
+        {
+            id: 20,
+            title: "Second",
+            daysLeft: 20, 
+            amount: 8000,
+            goal: 10000,
+            percentage: 80,
+        },
+        {
+            id: 30,
+            title: "Second",
+            daysLeft: 20, 
+            amount: 8000,
+            goal: 10000,
+            percentage: 80,
+        }
+    ],
+};
