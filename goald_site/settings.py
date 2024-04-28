13d567819220db2d67c8f8942718377a71dcb64d
Timur <Davletshin.TI@phystech.edu>
2024-04-28 23:22:03 +0300

    postgresql added

--- goald_site/settings.py
+++ goald_site/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres'
 ]
 
 MIDDLEWARE = [
@@ -80,9 +81,13 @@ WSGI_APPLICATION = 'goald_site.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "goald_db",
+        "USER": "goald",
+        "PASSWORD": "goald",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
