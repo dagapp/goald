222897efb3e230e0df6cb19600677808af298e25
iladygin <71135331+iladygin@users.noreply.github.com>
2024-04-28 22:06:01 +0300

    Fixed manager.py:92:0: R0902: Too many instance attributes (8/7)

--- goald_app/manager.py
+++ goald_app/manager.py
@@ -98,8 +98,7 @@ class GoalResult:
     is_active: bool
     deadline: str
     alert_period: str
-    current_value: int
-    final_value: int
+    values: Tuple[int, int]
     events: List[EventResult]
     reports: List[ReportResult]
 
@@ -108,11 +107,19 @@ class GoalResult:
         self.is_active = goal.is_active
         self.deadline = str(goal.deadline)
         self.alert_period = str(goal.alert_period)
-        self.current_value = sum(duty.current_value
-                                 for duty in goal.duties_goal.all())
-        self.final_value = sum(duty.final_value for duty in goal.duties_goal.all())
+        self.values = self.calculate_values(goal)
         self.events = [EventResult(event) for event in goal.events_goal.all()]
         self.reports = [ReportResult(report) for report in goal.reports_goal.all()]
+
+    def calculate_values(self, goal: Goal) -> Tuple[int, int]:
+        """
+        Return tuple of current and final values
+        """
+        current_value = sum(duty.current_value
+                                 for duty in goal.duties_goal.all())
+        final_value = sum(duty.final_value for duty in goal.duties_goal.all())
+
+        return current_value, final_value
 
 
 @dataclass
