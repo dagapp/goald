c6fe881e1c304ff9c93b81e5687d18a776202343
iladygin <71135331+iladygin@users.noreply.github.com>
2024-04-29 14:49:09 +0300

    report.py pylint fixs

--- goald_app/views/report.py
+++ goald_app/views/report.py
@@ -9,18 +9,24 @@ from ..models import User, Report
 from ..serializers import ReportSerializer
 
 class ReportView(APIView):
+    """
+        Description of ReportView
+    """
     def get(self, request):
         """
         Handler for reading the report info
         """
 
-        if not User.objects.get(id=request.session["id"]).groups.goals.reports.filter(id=request.GET["id"]).exists():
+        user_id = request.session["id"]
+        report_id = request.GET["id"]
+
+        if not User.objects.get(id=user_id).groups.goals.reports.filter(id=report_id).exists():
             return Response(
                 data={"detail": "You have no permission to have this info"},
                 status=status.HTTP_401_UNAUTHORIZED
             )
 
-        report = Report.objects.get(id=request.GET["id"])
+        report = Report.objects.get(id=report_id)
         return Response(
             data=ReportSerializer(instance=report).data,
             status=status.HTTP_200_OK
@@ -43,13 +49,12 @@ class ReportView(APIView):
                 data={"detail": "Report data is not valid"},
                 status=status.HTTP_400_BAD_REQUEST
             )
-        
+
         report.save()
         return Response(
             data={"detail", f"Report id: {report.data['id']}"},
             status=status.HTTP_201_CREATED
         )
-        
 
     def patch(self, request):
         """
@@ -58,19 +63,22 @@ class ReportView(APIView):
 
         report = ReportSerializer(data=request.data)
 
-        if not Report.objects.filter(id=request.GET["id"]).exists():
+        user_id = request.session["id"]
+        report_id = request.GET["id"]
+
+        if not Report.objects.filter(id=report_id).exists():
             return Response(
                 data={"detail": "Report with given id does not exist"},
                 status=status.HTTP_404_NOT_FOUND
             )
-        
-        if not User.obejcts.filter(id=request.session["id"]).groups.goals.reports.filter(id=request.GET["id"]).exists():
+
+        if not User.obejcts.filter(id=user_id).groups.goals.reports.filter(id=report_id).exists():
             return Response(
                 data={"detail": "You have no permission to change report info"},
                 status=status.HTTP_401_UNAUTHORIZED
             )
 
-        Report.objects.get(id=request.GET["id"]).update(report)
+        Report.objects.get(id=report_id).update(report)
 
         return Response(
             data={"detail": "Report info updated"},
@@ -83,19 +91,22 @@ class ReportView(APIView):
         Handler for deleting the report
         """
 
-        if not Report.objects.filter(id=request.GET["id"]).exists():
+        user_id = request.session["id"]
+        report_id = request.GET["id"]
+
+        if not Report.objects.filter(id=report_id).exists():
             return Response(
                 data={"detail": "Report with given id does not exist"},
                 status=status.HTTP_404_NOT_FOUND
             )
-        
-        if not User.obejcts.filter(id=request.session["id"]).groups.goals.reports.filter(id=request.GET["id"]).exists():
+
+        if not User.obejcts.filter(id=user_id).groups.goals.reports.filter(id=report_id).exists():
             return Response(
                 data={"detail": "You have no permission to delete this report"},
                 status=status.HTTP_401_UNAUTHORIZED
             )
-        
-        Report.objects.get(id=request.GET["id"]).delete()
+
+        Report.objects.get(id=report_id).delete()
 
         return Response(
             data={"detail": "Report deleted"},
