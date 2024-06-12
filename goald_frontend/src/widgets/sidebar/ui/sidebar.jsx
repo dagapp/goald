c16f61872ac0297aa94487b77e99ffd387cea5be
uf00led <dmitriimail8@gmail.com>
2024-06-13 02:42:26 +0300

    Fixing sidebar extension appearing

--- goald_frontend/src/widgets/sidebar/ui/sidebar.jsx
+++ goald_frontend/src/widgets/sidebar/ui/sidebar.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 import { Button } from "@shared/ui/button";
 import { Search } from "@features/search";
 
 import Logo from "@shared/assets/icons/logo.svg";
+import Analytics from "@shared/assets/icons/analytics.svg";
 import Groups from "@shared/assets/icons/groups.svg";
+
+import { getGroupById, getGoalsByGroupId } from "@shared/api/group";
 
 import "./sidebar.scss";
 
@@ -18,6 +23,30 @@ function SidebarExtension(props) {
 }
 
 export function Sidebar() {
+  const [extensionTitle, setExtensionTitle] = useState("");
+  const [innerComponent, setInnerComponent] = useState();
+
+  const renderAnalytics = () => {
+    setExtensionTitle("Analytics");
+
+    // Move to the features
+    // setInnerComponent(
+    //   <div className="test">
+    //     <Button Icon={Analytics} className={"sidebar__menu__button"}>
+    //       Lorem Ipsum
+    //     </Button>
+    //     <Button Icon={Analytics} className={"sidebar__menu__button"}>
+    //       Lorem Ipsum Ipsum
+    //     </Button>
+    //   </div>
+    // );
+  };
+
+  const renderGroups = () => {
+    setExtensionTitle("My Groups");
+    setInnerComponent(<Search />);
+  };
+
   return (
     <div className="sidebar-wrapper">
       <div className="sidebar">
@@ -25,19 +54,24 @@ export function Sidebar() {
           <Logo />
         </div>
         <nav className="sidebar__menu">
+          {/* <Button
+            Icon={Analytics}
+            className={"sidebar__menu__button"}
+            onClick={renderAnalytics}
+          /> */}
           <Button
             Icon={Groups}
             className={"sidebar__menu__button"}
-            onClick={() => {}}
-          />
-          <Button
-            Icon={Groups}
-            className={"sidebar__menu__button"}
-            onClick={() => {}}
+            onClick={renderGroups}
           />
         </nav>
       </div>
-      <SidebarExtension title={"Group"} innerComponent={<Search />} />
+      {extensionTitle && (
+        <SidebarExtension
+          title={extensionTitle}
+          innerComponent={innerComponent}
+        />
+      )}
     </div>
   );
 }
